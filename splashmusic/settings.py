a67ee96c938e98a4e0a31e2c1a8fef4e2c39c315
BlueCadet-3 <martysilha@gmail.com>
2020-12-23 14:58:38 -0600

    create and migrate database

--- splashmusic/settings.py
+++ splashmusic/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'splashmusic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'splashmusic',
+        'USER': 'django',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
