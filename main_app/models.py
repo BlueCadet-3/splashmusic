8ae7ca6f27996b56b57e8106955aca4ad9c61efa
gkutieva <73370477+gkutieva@users.noreply.github.com>
2020-12-23 18:17:08 -0500

    migrate profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    is_teacher = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
